7d248a8f1a0aae0040793ff7d027013d5ab55828
Alex <monsalverosas.alexander@gmail.com>
2024-05-02 19:31:48 -0400

    Conditions Frontieres finies

--- main.py
+++ main.py
@@ -4,30 +4,35 @@ import matplotlib.pyplot as plt
 
 #Grid, pas h = 0.1 mm, plan r(-3 mm à 3mm) et z(0 mm à 12 mm)
 h = 0.1
-r = np.linspace(-3, 3, int(6 / h) + 1)  # Adjusting the number of points to match h
-z = np.linspace(0, 12, int(12 / h) + 1)
+r = np.linspace(-3, 3, int(6 / h)) 
+z = np.linspace(0, 12, int(12 / h))
 
 R,Z = np.meshgrid(r,z) 
 
 z_9 = int(9 / h)
-z_7_5 = int(7.5/h)
+z_7_5 = int(7.5/h) 
 
 #Intiialiser la grid à 0 partout
-grid = np.zeros_like(Z)
+#grid = np.zeros_like(Z)
+grid = np.full_like(Z, 30)
+
+
 
 #Mettre des conditions frontieres faciles
 grid[:z_9,0] = -300
 grid[:z_9,-1] = -300
-grid[0,:] = 50
-grid[0:z_7_5,r == 0] = 50
-
-
+grid[0,:] = 0
+grid[0:z_7_5,r == 0] = 0
 
 # Cf Triangle 
-np.fill_diagonal(grid, 50)
+np.fill_diagonal(grid[z_9:,:], -300)
+np.fill_diagonal(np.fliplr(grid[z_9:,:]), -300)
+
+
+
 
 plt.figure(figsize=(8, 6))
-plt.imshow(grid, extent=[r[0], r[-1], z[0], z[-1]], cmap='jet', aspect='auto', origin='lower')
+plt.imshow(grid, extent=[r[0], r[-1], z[0], z[-1]], cmap='plasma', aspect='auto', origin='lower')
 plt.colorbar(label='Voltage (V)')
 plt.title('Voltage Distribution on the Grid')
 plt.xlabel('r (mm)')
