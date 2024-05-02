0ddbe46b90892bdbe923845c5c9fb55b44c7a118
Alex <monsalverosas.alexander@gmail.com>
2024-05-02 16:13:56 -0400

    Grid Faite

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-#allo alex
+from numpy import pi, cos, sin, cosh, sinh
+import numpy as np
+import pylab as plt
 
-def allo():
-    print("allo")
 
-allo()
+#Grid, pas h = 0.1 mm, plan r(-3 mm à 3mm) et z(0 mm à 12 mm)
 
-#oui?
+r = np.linspace(0,12,(12/0.1))
+z = np.linspace(-3,3, 6/0.1)
+r,z = np.meshgrid(r,z) 
