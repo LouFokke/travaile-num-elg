dcd853f210422a21347e2e77919b5724cbb0de41
Lou Cloutier <loclo66@ulaval.ca>
2024-05-03 18:06:13 -0400

    Q3 complété

--- num3.py
+++ num3.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import sys
 import numpy as np
 #np.set_printoptions(threshold=sys.maxsize)
@@ -78,3 +80,115 @@ mopdif2 = np.delete(mopdif1, range(14), 1)
 print('mopdif2 =', mopdif2)
 
 print('potentiel finale =', np.matmul(mopdif2, np.transpose(pot_for_point)))
+
+
+# Charger l'image depuis votre ordinateur
+image = mpimg.imread("C:/Users/louxc/OneDrive/Desktop/projet-num-elmg/travaile-num-elg/Q3bg.JPG")
+
+# Créer une matrice pour contenir les valeurs affichées aux intersections des lignes
+# Remplissez cette matrice avec vos données
+contenu_matrice = [
+    ['', '', '-300', '-300', '-300', '-300', '-300', '-300', ''],
+    ['', '-300', '-240.60', '-174.08', '-155.74', '-148.86', '-139.70', '-109.92', '0'],
+    ['-300', '-272.08', '-188.32', '0', '0', '0', '0', '0', ''],
+    ['', '-300', '-240.60', '-174.08', '-155.74', '-148.86', '-139.70', '-109.92', '0'],
+    ['', '', '-300', '-300', '-300', '-300', '-300', '-300', '']
+]
+
+
+
+# Dimensions de l'image
+largeur_image = 115  # Par exemple, une largeur de 10 unités
+
+# Calculer la hauteur de l'image proportionnellement à la largeur
+hauteur_image = largeur_image * image.shape[0] / image.shape[1]
+
+# Créer une grille de points pour les lignes verticales
+x = np.linspace(0, largeur_image, 10)
+
+# Créer une grille de points pour les lignes horizontales
+y = np.linspace(0, hauteur_image, 6)
+
+# Créer le graphique
+plt.figure()
+
+# Afficher l'image en arrière-plan avec les dimensions spécifiées
+plt.imshow(image, extent=[0, largeur_image, 0, hauteur_image])
+
+# Dessiner les lignes verticales grises
+for i in range(len(x)):
+    plt.plot([x[i], x[i]], [0, hauteur_image], color='gray')
+
+# Dessiner les lignes horizontales grises
+for j in range(len(y)):
+    plt.plot([0, largeur_image], [y[j], y[j]], color='gray')
+
+
+# Taille de police
+taille_police_1 = min(largeur_image, hauteur_image) * 0.15
+
+# Afficher le contenu de la matrice aux intersections des lignes
+for i in range(len(x) - 1):
+    for j in range(len(y) - 1):
+        plt.text((x[i] + x[i+1]) / 2, (y[j] + y[j+1]) / 2, contenu_matrice[j][i],
+                 horizontalalignment='center', verticalalignment='center', color = 'gray', fontsize=taille_police_1)
+
+# Paramètres d'affichage
+plt.xlim(0, largeur_image)
+plt.ylim(0, hauteur_image)
+plt.axis('off')
+
+# Afficher le graphique
+plt.show()
+
+
+noeud_matrice = [
+    ['', '', '17', '18', '19', '20', '21', '22', ''],
+    ['', '16', '9', '10', '11', '12', '13', '14', '23'],
+    ['15', '7', '8', '28', '27', '26', '25', '24', ''],
+    ['', '36', '1', '2', '3', '4', '5', '6', '29'],
+    ['', '', '35', '34', '33', '32', '31', '30', '']
+]
+
+# Dimensions de l'image
+largeur_image = 115  # Par exemple, une largeur de 10 unités
+
+# Calculer la hauteur de l'image proportionnellement à la largeur
+hauteur_image = largeur_image * image.shape[0] / image.shape[1]
+
+# Créer une grille de points pour les lignes verticales
+x = np.linspace(0, largeur_image, 10)
+
+# Créer une grille de points pour les lignes horizontales
+y = np.linspace(0, hauteur_image, 6)
+
+# Créer le graphique
+plt.figure()
+
+# Afficher l'image en arrière-plan avec les dimensions spécifiées
+plt.imshow(image, extent=[0, largeur_image, 0, hauteur_image])
+
+# Dessiner les lignes verticales grises
+for i in range(len(x)):
+    plt.plot([x[i], x[i]], [0, hauteur_image], color='gray')
+
+# Dessiner les lignes horizontales grises
+for j in range(len(y)):
+    plt.plot([0, largeur_image], [y[j], y[j]], color='gray')
+
+# Taille de police
+taille_police_2 = min(largeur_image, hauteur_image) * 0.4
+
+# Afficher le contenu de la matrice aux intersections des lignes
+for i in range(len(x) - 1):
+    for j in range(len(y) - 1):
+        plt.text((x[i] + x[i+1]) / 2, (y[j] + y[j+1]) / 2, noeud_matrice[j][i],
+                 horizontalalignment='center', verticalalignment='center', color = 'gray', fontsize=taille_police_2)
+
+# Paramètres d'affichage
+plt.xlim(0, largeur_image)
+plt.ylim(0, hauteur_image)
+plt.axis('off')
+
+# Afficher le graphique
+plt.show()
