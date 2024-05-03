4b0e26e66addbaf4de9ba6f5d9a268d8036269e3
Alex <monsalverosas.alexander@gmail.com>
2024-05-03 13:02:01 -0400

    Code symmetrique

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from numpy import pi, cos, sin, cosh, sinh
-import numpy as np
-import matplotlib.pyplot as plt
-
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -20,9 +16,13 @@ for i in range(61):
         if poten[i,j] == -300:
             break
         else:
-            poten[i,j] = 100      # Ici les points avec 50 on veux les eliminer apres
+            poten[i,j] = -600      # Ici les points avec 50 on veux les eliminer apres
 
 
+
+plt.imshow(poten, cmap='plasma')
+plt.colorbar()
+plt.show()
 
 
 
@@ -31,11 +31,15 @@ def calcul(pot, n_iter):
         pot_last = pot.copy()
         for x in range(121):
             for y in range(61):
-                if pot[y, x] != 0 and pot[y, x] != 100 and pot[y,x] != -300: #Le potentiel seulement en dedans du detecteur
+                if pot[y, x] != 0 and pot[y, x] != -600 and pot[y,x] != -300: #Le potentiel seulement en dedans du detecteur
                     if y == 30:
                         pot[y,x] = 0.5*(pot[y,x + 1]+pot[y, x -1])
                     else:
-                        pot[y,x] = 0.25*(pot[y,x + 1]+pot[y, x -1] + pot[y +1,x]+pot[y-1, x]) #+ (pot[y + 1,x]+pot[y-1, x]/(8 * y))  Faut tu mettre le h lors de la somme?
+                        #pot[y,x] = 0.25*(pot[y,x + 1]+pot[y, x -1] + pot[y +1,x]+pot[y-1, x]) + 0.1 * (pot[y + 1,x]-pot[y-1, x])/(8 * (abs(y - 30))) #Faut tu mettre le h lors de la somme?
+                        if y < 30:
+                            sym_y = 60 - y  # Finding the symmetrical point across y=30
+                            pot[y, x] = 0.25*(pot[y,x + 1]+pot[y, x -1] + pot[y +1,x]+pot[y-1, x]) + 0.1 * (pot[y + 1,x]-pot[y-1, x])/(8 * (abs(y - 30)))
+                            pot[sym_y, x] = pot[y, x]  # Setting the symmetrical point with the same potential
     return pot                                                                                # Deuxieme terme ne fonctionne pas
 
 
@@ -45,11 +49,11 @@ poten = calcul(poten, 1000)
 # Plotting the result
 
 # Masking the array to not display points equal to 50
-masked_poten = np.ma.masked_where(poten == 100, poten)
+masked_poten = np.ma.masked_where(poten == -600, poten)
 
 # Plotting the result using contourf
 plt.figure(figsize=(10, 6))
-plt.contourf(masked_poten, cmap='plasma', levels = 70)
+plt.contourf(masked_poten, cmap='plasma', levels = 100)
 plt.colorbar(label='Potential')
 plt.title('Electric Potential')
 plt.xlabel('X')
@@ -57,3 +61,14 @@ plt.ylabel('Y')
 plt.grid(True)
 plt.show()
 
+
+
+
+"""
+masked = np.ma.masked_where(poten == 100, poten)
+cmap = matplotlib.cm.spring
+cmap.set_bad(color = 'white')
+plt.imshow(masked, cmap='plasma')
+plt.colorbar()
+plt.show()
+"""
